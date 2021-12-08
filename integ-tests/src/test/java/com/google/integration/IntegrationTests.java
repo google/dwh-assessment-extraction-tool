410576ef27d491baaa6da7ce0afdff28bc0a0d03
Danila Morokov <dmorokov@yandex.com>
2021-12-08 23:25:30 +0000

    A single dummy test for testing Kokoro integration with Maven test run.

--- integ-tests/src/test/java/com/google/integration/IntegrationTests.java
+++ integ-tests/src/test/java/com/google/integration/IntegrationTests.java
@@ -1,5 +1,14 @@
 package com.google.integration;
 
+import org.junit.Test;
+
 public class IntegrationTests {
 
+    @Test
+    public void integrationTest() {
+        System.out.println(new Object() {}
+                .getClass()
+                .getEnclosingMethod()
+                .getName());
+    }
 }
