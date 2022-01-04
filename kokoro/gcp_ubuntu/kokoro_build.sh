b386930bdfbc22c8c4ab559f2c85b5452cdd9b51
DChernov <dmytro_chernov@outlook.com>
2022-01-04 14:48:46 -0500

    remove debug option

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -15,8 +15,6 @@ GCP_SCOPES="https://www.googleapis.com/auth/devstorage.read_only,https://www.goo
 "https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,"\
 "https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append"
 
-# Display commands being run.
-set -x
 
 gcloud components update
 
