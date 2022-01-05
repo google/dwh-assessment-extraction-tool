3e6fb2688e071f8781893a61e83c05d825d7e866
DChernov <dmytro_chernov@outlook.com>
2022-01-05 12:38:29 -0500

    Debug

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -59,7 +59,7 @@ rm -r /home/kbuilder/.cache/bazel/_bazel_kbuilder/install/4cfcf40fe067e89c8f5c38
 
 #bazel build dist:all
 bazel build dist:all
-ls -la
+ls -LR
 
 #./dwh-assessment-extraction-tool.sh td-extract --db-address jdbc:teradata://teradata-kokoro/DBS_PORT=1025,DATABASE=dbc --output ./output  --db-user "${TD_PSW}" --db-password "${TD_PSW}"
 
