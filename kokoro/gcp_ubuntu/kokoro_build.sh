38fbf35ffae1930597f54a19bd7199941e1cdcfd
dmytro-chernov <93734839+dmytro-chernov@users.noreply.github.com>
2022-01-12 14:48:04 -0500

    Add Kokoro config and build files (#34)
    * Add Kokoro config and build files

---
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+#
+# A Kokoro build script to run the extraction tool and integration tests
+
+# Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
+# The final directory name in this path is determined by the scm name specified
+# in the job configuration.
+
+# Display commands being run.
+# WARNING: please only enable 'set -x' if necessary for debugging, and be very
+#  careful if you handle credentials (e.g. from Keystore) with 'set -x':
+#  statements like "export VAR=$(cat /tmp/keystore/credentials)" will result in
+#  the credentials being printed in build logs.
+#  Additionally, recursive invocation with credentials as command-line
+#  parameters, will print the full command, with credentials, in the build logs.
+# set -x
+
+# Fail on any error.
+set -e
+
+termInstance() {
+  gcloud compute instances delete "${TD_HOST}" --zone us-central1-a --project="${GCP_PROJECT}" --quiet
+}
+
+#delete instance after error
+trap 'termInstance' ERR
+
+#Variables
+export KOKORO_PROJECT_NAME="${KOKORO_JOB_NAME##*/}"
+export TD_PSW="$(<${KOKORO_KEYSTORE_DIR}/76474_teradata-12232021)"
+export TD_USR="dbc"
+export TD_DB="dbc"
+export TD_HOST="teradata-kokoro-${KOKORO_PROJECT_NAME}"
+export EXPORT_PATH="${KOKORO_ARTIFACTS_DIR}/github/dwh-assessment-extraction-tool/bazel-bin/dist/dwh-assessment-extraction-tool/output"
+export JDBC_PATH="${KOKORO_ARTIFACTS_DIR}/piper/google3/third_party/java/jdbc/teradata/terajdbc4.jar"
+export CLASSPATH="${JDBC_PATH}"
+export GCP_PROJECT="$(gcloud config get-value project)"
+export GCP_PROJECT_ID="$(gcloud projects describe ${GCP_PROJECT} --format 'value(projectNumber)')"
+export GCP_SERVICE_ACCOUNT="${GCP_PROJECT_ID}-compute@developer.gserviceaccount.com"
+export GCP_IMAGE="projects/${GCP_PROJECT}/global/images/teradata1610-ubuntu20"
+export GCP_SCOPES="https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,"\
+"https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,"\
+"https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append"
+
+gcloud components update
+
+#Create Kokoro Teradata instance
+gcloud compute instances create "${TD_HOST}" \
+    --project="${GCP_PROJECT}" \
+    --zone=us-central1-a \
+    --machine-type=n2-standard-4 \
+    --network-interface=subnet=default,no-address \
+    --maintenance-policy=MIGRATE \
+    --service-account="${GCP_SERVICE_ACCOUNT}" \
+    --scopes="${GCP_SCOPES}" \
+    --create-disk=auto-delete=yes,boot=yes,device-name="${TD_HOST}",image="${GCP_IMAGE}",mode=rw,size=300,type=projects/"${GCP_PROJECT}"/zones/us-central1-a/diskTypes/pd-balanced \
+    --reservation-affinity=any
+
+
+#Bugfix for bazel
+use_bazel.sh 4.1.0
+command -v bazel
+bazel version
+
+
+
+cd "${KOKORO_ARTIFACTS_DIR}/github/dwh-assessment-extraction-tool"
+
+#Build extraction tool
+bazel build dist:all
+
+cd "${KOKORO_ARTIFACTS_DIR}/github/dwh-assessment-extraction-tool/bazel-bin/dist/"
+unzip ./dwh-assessment-extraction-tool.zip
+
+#Time to allow TD to recover
+sleep 4m
+
+cd "${KOKORO_ARTIFACTS_DIR}/github/dwh-assessment-extraction-tool/bazel-bin/dist/dwh-assessment-extraction-tool"
+
+mkdir output
+
+
+#Extract data from TD instance
+set +x
+./dwh-assessment-extraction-tool td-extract \
+    --db-address jdbc:teradata://"${TD_HOST}"/DBS_PORT=1025,DATABASE="${TD_DB}" \
+    --output "${EXPORT_PATH}" \
+    --db-user "${TD_USR}" \
+    --db-password "${TD_PSW}"
+
+#delete instance after tests
+termInstance
