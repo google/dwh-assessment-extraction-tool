42405436ffdab8512fd007ba324fb44fbde7ce2b
Danila Morokov <morokov@google.com>
2022-01-20 00:27:37 +0000

    Moving test data generation to a dedicated class
    1. Test data generation can be called separately from tests
    2. Configured exec-maven-plugin for running TestDataGenerator.java

--- integ-tests/src/main/java/com/google/testdata/TestDataHelper.java
+++ integ-tests/src/main/java/com/google/testdata/TestDataHelper.java
@@ -3,15 +3,21 @@ package com.google.testdata;
 import static java.lang.String.format;
 import static java.lang.System.nanoTime;
 
+import com.google.base.TestBase;
+import com.google.common.base.Joiner;
 import com.google.sql.SqlHelper;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Helper class providing test data generation methods */
 public final class TestDataHelper {
+
+  private static final Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   private TestDataHelper() {}
 
@@ -27,5 +33,7 @@ public final class TestDataHelper {
     sqlQueries =
         sqlQueries.stream().map(e -> e = format(e, nanoTime())).collect(Collectors.toList());
     SqlHelper.executeQueries(connection, sqlQueries);
+    logger.info(
+        format("Generated %s new Users:\n%s", sqlQueries.size(), Joiner.on("\n").join(sqlQueries)));
   }
 }
