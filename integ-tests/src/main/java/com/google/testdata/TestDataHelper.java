ea3c7cc3f624faef1402bc566985e91e84391f09
Danila Morokov <morokov@google.com>
2022-01-20 23:11:40 +0000

    Masking User generation CLI output
    1. Masking User's password
    2. Adding Apache commons-lang3

--- integ-tests/src/main/java/com/google/testdata/TestDataHelper.java
+++ integ-tests/src/main/java/com/google/testdata/TestDataHelper.java
@@ -1,23 +1,24 @@
 package com.google.testdata;
 
+import static com.google.common.base.Suppliers.memoize;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static java.lang.String.format;
 import static java.lang.System.nanoTime;
 
-import com.google.base.TestBase;
 import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
 import com.google.sql.SqlHelper;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.UUID;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /** Helper class providing test data generation methods */
 public final class TestDataHelper {
 
-  private static final Logger logger = LoggerFactory.getLogger(TestBase.class);
+  private static final Logger logger = LoggerFactory.getLogger(TestDataHelper.class);
 
   private TestDataHelper() {}
 
@@ -28,12 +29,19 @@ public final class TestDataHelper {
    */
   public static void generateUsers(Connection connection, int userCount) throws SQLException {
     final String sqlDataPath = "src/main/java/com/google/sql/data/users_data.sql";
+    final String password = UUID.randomUUID().toString();
 
-    List<String> sqlQueries = Collections.nCopies(userCount, SqlHelper.getSql(sqlDataPath));
-    sqlQueries =
-        sqlQueries.stream().map(e -> e = format(e, nanoTime())).collect(Collectors.toList());
+    ImmutableList<String> sqlQueries =
+        Stream.generate(memoize(() -> SqlHelper.getSql(sqlDataPath)))
+            .limit(userCount)
+            .map(sqlQuery -> format(sqlQuery, nanoTime(), password))
+            .collect(toImmutableList());
+
     SqlHelper.executeQueries(connection, sqlQueries);
+
     logger.info(
-        format("Generated %s new Users:\n%s", sqlQueries.size(), Joiner.on("\n").join(sqlQueries)));
+        format(
+            "Generated %s new Users:\n%s",
+            sqlQueries.size(), Joiner.on("\n").join(sqlQueries).replaceAll(password, "####")));
   }
 }
