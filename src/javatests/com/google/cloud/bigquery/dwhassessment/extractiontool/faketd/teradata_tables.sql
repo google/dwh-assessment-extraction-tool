9f038ba893b555c01dc378fe6dc42748c9b6d4ad
Roman Fursov <rfursov@google.com>
2022-02-22 13:46:42 +0100

    Revert changes from using QryLogObjectsV instead of DBQLObjTbl
    TypeofUse field in QryLogObjectsV has different type than DBQLObjTbl and provides an explanation over possible values, e.g. "70: Access, conditional, full outer join" instead of 70. We have similar functionality to map bit values to types during assessment, so I revert everything back to use DBQLObjTbl raw bit values.

--- src/javatests/com/google/cloud/bigquery/dwhassessment/extractiontool/faketd/teradata_tables.sql
+++ src/javatests/com/google/cloud/bigquery/dwhassessment/extractiontool/faketd/teradata_tables.sql
@@ -342,7 +342,7 @@ CREATE TABLE DBC."PartitioningConstraintsV" (
   "ColumnPartitioningLevel" SMALLINT NOT NULL,
 );
 
-CREATE TABLE DBC."QryLogObjectsV" (
+CREATE TABLE DBC."DBQLObjTbl" (
   "ProcID" DECIMAL(5,0) NOT NULL,
   "CollectTimeStamp" TIMESTAMP(6) NOT NULL,
   "QueryID" DECIMAL(18,0) NOT NULL,
