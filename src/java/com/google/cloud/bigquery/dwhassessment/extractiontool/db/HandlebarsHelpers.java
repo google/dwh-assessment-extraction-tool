9e90c19d48cab97d74750449a7a5f1b149f363de
Roman Fursov <rfursov@google.com>
2022-04-23 09:28:15 +0200

    Review fixes.

--- src/java/com/google/cloud/bigquery/dwhassessment/extractiontool/db/HandlebarsHelpers.java
+++ src/java/com/google/cloud/bigquery/dwhassessment/extractiontool/db/HandlebarsHelpers.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.cloud.bigquery.dwhassessment.extractiontool.db;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -32,8 +47,8 @@ public final class HandlebarsHelpers {
     if (!queryLogsVariables.users().isEmpty()) {
       whereClauses.add(
           String.format(
-              "\"%s\".\"UserName\" IN %s",
-              qryLogVAlias,
+              "%s.\"UserName\" IN %s",
+              wrapInQuotes(qryLogVAlias),
               queryLogsVariables.users().stream().collect(Collectors.joining("','", "('", "')"))));
     }
     if (!whereClauses.isEmpty()) {
